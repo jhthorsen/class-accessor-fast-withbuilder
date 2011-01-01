cd2f699a0252db46a2778254fceaf945eea1cd79
Jan Henning Thorsen <jhthorsen@cpan.org>
2011-01-01 20:19:42 +0100

    init Class::Accessor::Fast::WithBuilder

---
+++ lib/Class/Accessor/Fast/WithBuilder.pm
@@ -0,0 +1,36 @@
+package Class::Accessor::Fast::WithBuilder;
+
+=head1 NAME
+
+Class::Accessor::Fast::WithBuilder - Class::Accessor::Fast with lazy defaults
+
+=head1 VERSION
+
+0.00
+
+=head1 DESCRIPTION
+
+=head1 SYNOPSIS
+
+    use base qw/Class::Accessor::Fast::WithBuilder/;
+
+=cut
+
+use strict;
+use warnings;
+
+our $VERSION = eval '0.00';
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 COPYRIGHT & LICENSE
+
+=head1 AUTHOR
+
+Jan Henning Thorsen C<< jhthorsen at cpan.org >>
+
+=cut
+
+1;
---
+++ t/00-load.t
@@ -0,0 +1,4 @@
+use lib qw(lib);
+use Test::More;
+plan tests => 1;
+use_ok('Class::Accessor::Fast::WithBuilder');
